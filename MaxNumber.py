2e76e85e97895d5c40ec6a39f9c2a95496d63a75
Evashen-123 <36735944+Evashen-123@users.noreply.github.com>
2018-02-24 12:53:00 +0200

    Max Number
    Finds the max number

---
+++ MaxNumber.py
@@ -0,0 +1,8 @@
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+z=int(input("enter third number:"))
+
+print("the max number is:")
+print(max(x,y,z))
+
+input("please press enter to exit")
